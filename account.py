dff547e624f90f805b04a5321ae41146f773e6da
Jaqueline Monteiro <jaqueline.monteiro2012@gmail.com>
2023-08-10 22:53:36 +0000

    Implementing private and static methods

--- account.py
+++ account.py
@@ -1,4 +1,4 @@
-class Account: 
+class Account:
 
     def __init__(self, number, holder, balance, limit):
         print("Building object ... {}".format(self))
@@ -7,15 +7,46 @@ class Account:
         self.__balance = balance
         self.__limit = limit
 
-    def extract(self):
-        print("Balance of {} of holder {}".format(self.__balance, self.__holder))
+    def statement(self):
+        print("Balance of {} for account holder {}".format(self.__balance, self.__holder))
 
-    def deposit(self, amount):
-        self.__balance += amount
+    def deposit(self, value):
+        self.__balance += value
 
-    def withdraw(self, amount):
-        self.__balance -= amount
+    def __can_withdraw(self, value_to_withdraw):
+        available_withdrawal_value = self.__balance + self.__limit
+        return value_to_withdraw <= available_withdrawal_value
 
-    def transfer(self, amount):
-        self.withdraw(amount)
-        self.deposit(amount)
+    def withdraw(self, value):
+        if(self.__can_withdraw(value)):
+            self.__balance -= value
+        else:
+            print("The value {} exceeded the limit".format(value))
+
+    def transfer(self, value, destination):
+        self.withdraw(value)
+        destination.deposit(value)
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @property
+    def get_holder(self):
+        return self.__holder
+
+    @property
+    def limit(self):
+        return self.__limit
+
+    @limit.setter
+    def limit(self, limit):
+        self.__limit = limit
+
+    @staticmethod
+    def bank_code():
+        return "001"
+
+    @staticmethod
+    def bank_codes():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
